f2d6e9b03e677b5917e18b084b079000ad5e8a09
umairnaeem373 <umairnaeem373@gmail.com>
2024-05-15 00:33:33 +0500

    Initial Commit

---
+++ src/content_option.js
@@ -0,0 +1,160 @@
+const logotext = "Mʌʀɩ";
+const meta = {
+    title: "Umair Naeem",
+    description: "I’m M.Umair Naeem,a MERN stack developer,currently working in Lahore",
+};
+
+const introdata = {
+    title: "I’m Mʌʀɩ Bʌɓʌ",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop web apps",
+    },
+    description: "Explore my portfolio showcasing diverse projects and skills in web development. Let's create innovative solutions together. Welcome to my site",
+    your_img_url: "https://scontent.flhe9-1.fna.fbcdn.net/v/t39.30808-6/325661324_1835488783453670_8896718358011540436_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeET6Mc7yPAfA2use-ZzY-BbkrT15zdnVG-StPXnN2dUb9J2OkarRaOaPv_jSDNP2jVJycZEQLZbBPYSMR3MSO4h&_nc_ohc=jdRRzrfN--IQ7kNvgEDRe8u&_nc_ht=scontent.flhe9-1.fna&oh=00_AYCnLAwe1ZZOUyiZsgcb0w77O7EDApQZQaCCJJZdNGu7lA&oe=66484D80",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Welcome to my portfolio! I'm a MERN stack web developer passionate about crafting intuitive and efficient web solutions. Explore my projects and skills to learn more about my work.",
+};
+const worktimeline = [{
+    jobtitle: "Fron End Developer",
+    where: "VCT",
+    date: "2023",
+},
+];
+
+const skills = [{
+    name: "Html",
+    value: 90,
+},
+{
+    name: "Css",
+    value: 85,
+},
+{
+    name: "Javascript",
+    value: 80,
+},
+{
+    name: "Reactjs",
+    value: 70,
+},
+{
+    name: "Nodejs",
+    value: 60,
+},
+{
+    name: "Expressjs",
+    value: 80,
+},
+{
+    name: "MongoDB",
+    value: 80,
+},
+];
+
+const services = [
+    {
+        title: "Web Apps",
+        description: "We offer custom web development, responsive design, e-commerce solutions, SEO optimization, and ongoing support to help your business thrive online.",
+    },
+    {
+        title: "Web Design",
+        description: "We specialize in creating custom web designs that are modern, user-friendly, responsive, and tailored to showcase your brand effectively online.",
+    },
+];
+
+const dataportfolio = [{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/800/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/550/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "umairnaeem373@gmail.com",
+    YOUR_FONE: "0334 1041181",
+    description: "Please feel free to reach out using the form below. Your message is important to us and we'll respond promptly. Thank you for contacting us!. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/umairnaeem373",
+    facebook: "https://www.facebook.com/umairnaeem373",
+    linkedin: "https://www.linkedin.com/in/umairnaeem373",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
